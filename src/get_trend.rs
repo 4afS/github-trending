c1656a4bd02559ecf5dc747a0f20a6becb60a459
4afS <4afs.h1@gmail.com>
2021-03-20 02:25:30 +0900

    use anyhow

--- src/get_trend.rs
+++ src/get_trend.rs
@@ -9,45 +9,45 @@ pub fn get_trend(document: Document) -> anyhow::Result<Vec<Trend>> {
     let mut result = Vec::new();
 
     for article in document.select("article.Box-row").iter() {
-        let user_repo = username_and_repo(&article)
-            .ok_or_else(|| TrendError::new("username and repository not found"))?;
+        let (username, repository) = username_and_repository(&article)?;
         result.push(Trend::new(
-            &user_repo.0,
-            &user_repo.1,
-            &about(&article).ok_or_else(|| TrendError::new("about repository nod found"))?,
-            Stars::new(
-                stars_count(&article)
-                    .ok_or_else(|| TrendError::new("total stars count not found"))?,
-                stars_count_in(&article)
-                    .ok_or_else(|| TrendError::new("stars count in date range not found"))?,
-            ),
-            forks_count(&article).ok_or_else(|| TrendError::new("forks count not found"))?,
+            &username,
+            &repository,
+            &about(&article)?,
+            Stars::new(stars_count(&article)?, stars_count_in(&article)?),
+            forks_count(&article)?,
         ));
     }
 
     Ok(result)
 }
 
-fn username_and_repo(article: &Selection) -> Option<(String, String)> {
+fn username_and_repository(article: &Selection) -> anyhow::Result<(String, String)> {
     let elements = article
         .select("h1:nth-child(2) > a:nth-child(1)")
         .text()
         .split('/')
         .map(|e| e.trim().to_string())
         .collect::<Vec<_>>();
-    return Some((elements.get(0)?.to_string(), elements.get(1)?.to_string()));
-}
 
-fn about(article: &Selection) -> Option<String> {
-    let about = article.select("p:nth-child(3)").text().trim().to_string();
-    if about.is_empty() {
-        None
-    } else {
-        Some(about)
+    match (elements.get(0), elements.get(1)) {
+        (None, None) => Err(TrendError::NotFound("username and repository".to_string()).into()),
+        (_, None) => Err(TrendError::NotFound("repository".to_string()).into()),
+        (None, _) => Err(TrendError::NotFound("username".to_string()).into()),
+        (Some(username), Some(repository)) => Ok((username.to_string(), repository.to_string())),
     }
 }
 
-fn number(article: &Selection, selector: &str) -> Option<u32> {
+fn about(article: &Selection) -> anyhow::Result<String> {
+    let about = article.select("p:nth-child(3)").text().trim().to_string();
+    if about.is_empty() {
+        Err(TrendError::NotFound("about".to_string()).into())
+    } else {
+        Ok(about)
+    }
+}
+
+fn number(article: &Selection, selector: &str, param_type: &str) -> anyhow::Result<u32> {
     let total_star_count = article
         .select(selector)
         .text()
@@ -55,21 +55,30 @@ fn number(article: &Selection, selector: &str) -> Option<u32> {
         .chars()
         .filter(|e| e.is_digit(10))
         .collect::<String>();
+
     if total_star_count.is_empty() {
-        None
+        return Err(TrendError::NotFound(param_type.to_string()).into());
+    }
+
+    if let Ok(number) = total_star_count.parse::<u32>() {
+        Ok(number)
     } else {
-        Some(total_star_count.parse::<u32>().ok()?)
+        Err(TrendError::ParseError.into())
     }
 }
 
-fn stars_count(article: &Selection) -> Option<u32> {
-    number(article, "div:nth-child(4) > a:nth-child(2)")
+fn stars_count(article: &Selection) -> anyhow::Result<u32> {
+    number(article, "div:nth-child(4) > a:nth-child(2)", "stars_count")
 }
 
-fn forks_count(article: &Selection) -> Option<u32> {
-    number(article, "div:nth-child(4) > a:nth-child(3)")
+fn forks_count(article: &Selection) -> anyhow::Result<u32> {
+    number(article, "div:nth-child(4) > a:nth-child(3)", "forks_count")
 }
 
-fn stars_count_in(article: &Selection) -> Option<u32> {
-    number(article, "div:nth-child(4) > span:nth-child(5)")
+fn stars_count_in(article: &Selection) -> anyhow::Result<u32> {
+    number(
+        article,
+        "div:nth-child(4) > span:nth-child(5)",
+        "stars_count_in",
+    )
 }
